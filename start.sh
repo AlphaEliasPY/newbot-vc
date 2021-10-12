ff6a6310195f989ca6773482d1677db5f5805d48
AlphaEliasPY <84801328+AlphaEliasPY@users.noreply.github.com>
2021-10-11 22:24:55 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 echo "Cloning Repo, Please Wait..."
-git clone https://github.com/AsmSafone/RadioPlayerV3.git /RadioPlayerV3
+git clone https://github.com/AlphaEliasPY/newbot-vc.git /newbot-vc
+
 echo "Installing Requirements..."
-cd /RadioPlayerV3
+cd /newbot-vc
+
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
 python3 main.py
